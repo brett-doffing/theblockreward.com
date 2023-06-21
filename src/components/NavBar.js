f11e11a8707996d3100472e7ecc8e5921bd88de0
Brett Doffing <doffing.brett@gmail.com>
2023-06-21 11:08:27 -0500

    Add hamburger for side menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,30 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { Divide as Hamburger } from 'hamburger-react'
 
 function NavBar() {
-  return (
-    <>
-        <nav className='navbar'>
-            <div className='navbar-container'>
-                <Link to='/' className='navbar-logo'>
-                    TBR
-                </Link>
-            </div>
-        </nav>
-    </>
-  )
+    const [sideShown, setSideShown] = useState(false)
+    return (
+        <>
+            <nav className='navbar'>
+                <div className='navbar-container'>
+                    <Link to='/' className='navbar-logo'>
+                        TBR
+                    </Link>
+                    <div className='menu-icon'>
+                    <Hamburger onToggle={toggled => {
+                        if (toggled) {
+                            // open a menu
+                        } else {
+                            // close a menu
+                        }
+                        }} 
+                    />
+                    </div>
+                </div>
+            </nav>
+        </>
+    )
 }
 
 export default NavBar
